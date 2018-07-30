771d43aa054c621a172fc693497c47064c3d583b
Jefferson Nascimento <jefferson.nascimento3@fatec.sp.gov.br>
2018-07-30 01:03:58 -0300

    Add react-grid-system

--- src/GithubSearch/Home.js
+++ src/GithubSearch/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import GithubSearchLogo from './Logo';
 import SearchBar from "./SearchBar";
-import { Container, Row, Col } from 'react-grid-system';
 
 const stylesForHome = {
     maxWidth: "750px",
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Home from './GithubSearch/Home';
+
+ReactDOM.render((
+    <Router>
+        <div>
+            <Route exact path="/" component={Home} />
+        </div>
+    </Router>
+    ), document.getElementById('root')
+);
+
 registerServiceWorker();
