ef61bebecd2a403d91343506f9236253e420a2b3
Jefferson Nascimento <jefferson.nascimento3@fatec.sp.gov.br>
2018-07-30 00:59:24 -0300

    Add Home component

---
+++ src/GithubSearch/Home.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import GithubSearchLogo from './Logo';
+import SearchBar from "./SearchBar";
+import { Container, Row, Col } from 'react-grid-system';
+
+const stylesForHome = {
+    maxWidth: "750px",
+    textAlign: "center",
+    margin: "0 auto",
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center"
+};
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="Home" style={stylesForHome}>
+                <div style={{ paddingBottom: "10rem" }}>
+                    <GithubSearchLogo />
+                    <SearchBar />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
