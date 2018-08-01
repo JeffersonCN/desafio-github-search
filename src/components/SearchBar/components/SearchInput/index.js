a45c90b293934bfac7308b6b17d175672e0d0bba
Jefferson Nascimento <jefferson.nascimento3@fatec.sp.gov.br>
2018-08-01 00:26:29 -0300

    Add SearchInput component

---
+++ src/components/SearchBar/components/SearchInput/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './styles.scss';
+
+class SearchInput extends Component {
+    render() {
+        return (
+            <input type="text" className="search-input"/>
+        );
+    }
+}
+
+export default SearchInput;
