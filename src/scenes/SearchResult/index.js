1c2a06f790b13f1ef8fe216a85122a1268065d44
Jefferson Nascimento <jefferson.nascimento3@fatec.sp.gov.br>
2018-08-01 00:25:42 -0300

    Add SearchResult component

---
+++ src/scenes/SearchResult/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './styles.scss';
+
+class SearchResult extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default SearchResult;
