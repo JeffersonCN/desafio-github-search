5cc7e8aeaff98346c123556aafe3c4941f469d77
Jefferson Nascimento <jefferson.nascimento3@fatec.sp.gov.br>
2018-08-02 20:37:24 -0300

    Add Route to SearchResult

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './scenes/Home';
+import SearchResult from './scenes/SearchResult';
 
 class App extends Component {
     render() {
@@ -9,7 +10,7 @@ class App extends Component {
             <Router>
                 <Switch>
                     <Route exact path="/" component={Home} />
-                    <Route path="/users/:login" component={Home} />
+                    <Route path="/users/:login" component={SearchResult} />
                 </Switch>
             </Router>
         );
